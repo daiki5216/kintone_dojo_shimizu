3f3a3026c18cb3684805ec55e0f53c5072f502bd
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-06 20:43:17 +0900

    課題9終了

---
+++ task_9/9-1.js
@@ -0,0 +1,29 @@
+(() => {
+    'use strict';
+    const appId = kintone.app.getId();
+    const params = {
+        "app":appId,
+        "field":"重複禁止項目"
+    }    
+
+    kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], (event) => {
+        return kintone.api(kintone.api.url('/k/v1/records.json',true),'GET',params).then((res)=>{
+            const allRecords = res.records
+            const recordContentsList =[]
+            allRecords.forEach((record) => {
+                recordContentsList.push(record.重複禁止項目.value)
+            });
+            const inputData = event.record.重複禁止項目.value
+            
+            if (recordContentsList.includes(inputData)){
+                const checkOverlap = confirm('レコードが重複しています。このまま保存しますか？');
+                if(!checkOverlap){
+                    return false
+                }  
+            }
+        })
+       
+    });
+
+    
+})();
