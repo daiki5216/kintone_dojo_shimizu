f6b0afc2d438c0afd6690e938d6e19044e8d8f75
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-07 21:17:06 +0900

    クラス付与方法を変更

--- task_5/scripts.js
+++ task_5/scripts.js
@@ -1,14 +1,4 @@
-function chageBackgroundColor(tdCategory,category){
-    if (category === "製品"){
-       tdCategory.classList.add("product");
-    }if(category === "企業情報"){
-        tdCategory.classList.add("company_info");
-    }if(category === "IR 情報"){
-        tdCategory.classList.add("ir_info");
-    } 
-};
-
-function getNews(){
+const getNews =()=>{
     axios.get("https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo",{
         params:{
             id:'dojo',
@@ -16,6 +6,7 @@ function getNews(){
         })
         .then((res)=>{
             const newsDatas=res.data
+            console.log(newsDatas)
             const tbody = document.getElementById('tbody');
 
             newsDatas.forEach((row) => {
@@ -27,7 +18,7 @@ function getNews(){
                 tdDay.textContent = row.day.value
 
                 tdCategory.textContent = row.category.value
-                chageBackgroundColor(tdCategory, row.category.value);
+                tdCategory.setAttribute('class',`${row.label.value}`)
 
                 tdContent.innerHTML=`<a href=${row.url.value} target=${row.target.value}>${row.content.value}</a>`
 
