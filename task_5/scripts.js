452766003ed1d82731f93004947832ef66838dbe
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-07 13:58:14 +0900

    課題5終了

--- task_5/scripts.js
+++ task_5/scripts.js
@@ -15,16 +15,38 @@ function getNews(){
     axios.get("https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo",{
         params:{
             id:'dojo',
-            //query:'day'
         }
         })
         .then((res)=>{
             const newsDatas=res.data
+            console.log(res.data)
             const nums =newsDatas.length
+            const tbody = document.getElementById('tbody')
 
-            for (let i =1; i<nums+1;i++){
-                const i_datas = res.data[i-1]
+            for (let i =0; i<nums;i++){                
+                const tr = document.createElement('tr')
+                const tdDay = document.createElement('td')
+                const tdCategory = document.createElement('td')
+                const tdContent = document.createElement('td')
 
+                const tdDayIdName = i+'-day'
+                const tdCategoryIdName = i+'-category'
+                const tdContentIdName = i+'-content'
+            
+                tr.appendChild(tdDay)
+                tr.appendChild(tdCategory)
+                tr.appendChild(tdContent)
+                tbody.appendChild(tr)
+
+                tdDay.setAttribute("id", tdDayIdName);
+                tdCategory.setAttribute("id", tdCategoryIdName);
+                tdContent.setAttribute("id", tdContentIdName);
+
+            }
+
+            for (let i =0;i<nums;i++){
+               //res.dataの値を入れていく処理↓↓
+                const i_datas = res.data[i]
                 document.getElementById(i+'-day').textContent= i_datas.day.value
 
                 const i_category = document.getElementById(i+'-category')
@@ -32,20 +54,10 @@ function getNews(){
                 chageBackgroundColor(i,i_category.textContent)
                 
                 const i_content = document.getElementById(i+'-content')
-                i_content.textContent= i_datas.content.value
-                i_content.href = i_datas.url.value
-                if (i_datas.target.value ==='_blank'){
-                    i_content.setAttribute('target', '_blank');
-                } 
-                
+                i_content.innerHTML=`<a href=${i_datas.url.value} target=${i_datas.target.value}>${i_datas.content.value}</a>`
 
             }
-
-       
-
-
         })
-
 };
 
 getNews();
