f2f5eb22d93ec20f567c2652a4e6d2c4b4bf526e
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-08 10:45:10 +0900

    セミコロンを追加

--- task_7/7-1.js
+++ task_7/7-1.js
@@ -11,30 +11,30 @@
         'app.record.edit.change.サイボウズ製品',
         'app.record.create.change.管理番号',
         'app.record.edit.change.管理番号'
-    ]
+    ];
 
     const products ={
         'kintone':'KN',
         'Garoon':'GR',
         'サイボウズ Office':'OF',
         'Mailwise':'MW'
-    }
+    };
 
     kintone.events.on(trigger, (event) => {
         event.record['重複禁止項目_文字列']['disabled'] = true;
-        const day = event.record['日付'].value
-        const Editedday = dateFns.format(day, 'YYYYMMDD')
+        const day = event.record['日付'].value;
+        const Editedday = dateFns.format(day, 'YYYYMMDD');
 
 
-        const product = event.record['サイボウズ製品'].value
-        const EditedProductName = products[product]
+        const product = event.record['サイボウズ製品'].value;
+        const EditedProductName = products[product];
 
-        const manageNum = event.record['管理番号'].value
+        const manageNum = event.record['管理番号'].value;
 
-        const inputData = Editedday+'-'+EditedProductName+'-'+manageNum
+        const inputData = Editedday+'-'+EditedProductName+'-'+manageNum;
 
-        event.record['重複禁止項目_文字列'].value = inputData
-        return event
+        event.record['重複禁止項目_文字列'].value = inputData;
+        return event;
     });
 
 
