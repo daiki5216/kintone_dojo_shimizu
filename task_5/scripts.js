c166998b547e52595a6f0ff88c87d9a8a290eea4
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-08 12:26:22 +0900

    インデントを整形

--- task_5/scripts.js
+++ task_5/scripts.js
@@ -1,21 +1,21 @@
 const getNews =()=>{
-    axios.get("https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo",{
-        params:{
-            id:'dojo',
-        }
-        })
-        .then((res)=>{
-            const newsDatas=res.data;
-            const tbody = document.getElementById('tbody');
+  axios.get("https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo",{
+    params:{
+        id:'dojo',
+      }
+    })
+    .then((res)=>{
+      const newsDatas=res.data;
+      const tbody = document.getElementById('tbody');
 
-            newsDatas.forEach((row) => {
-                const tr = document.createElement('tr');
-                tr.innerHTML=`
-                  <td>${row.day.value}</td>
-                  <td class=${row.label.value}>${row.category.value}</td>
-                  <td><a href=${row.url.value} target=${row.target.value}>${row.content.value}</a></td>`;
-                tbody.appendChild(tr);
-            });
-        });
+      newsDatas.forEach((row) => {
+        const tr = document.createElement('tr');
+        tr.innerHTML=`
+          <td>${row.day.value}</td>
+          <td class=${row.label.value}>${row.category.value}</td>
+          <td><a href=${row.url.value} target=${row.target.value}>${row.content.value}</a></td>`;
+        tbody.appendChild(tr);
+      });
+    });
 };
 getNews();
