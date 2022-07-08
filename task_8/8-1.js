a2e94154773deb21fcd9a72668b95c233ce50d6c
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-08 16:32:52 +0900

    インデントを変更

--- task_8/8-1.js
+++ task_8/8-1.js
@@ -1,43 +1,43 @@
 (() => {
-    'use strict';
-    
-    kintone.events.on('app.record.create.show', (event) => {
-        const appId = kintone.app.getId();
-        const params = {
-            "app":appId
+  'use strict';
+
+  kintone.events.on('app.record.create.show', (event) => {
+    const appId = kintone.app.getId();
+    const params = {
+        "app":appId
+    };
+    return   kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', params).then((resp) => {
+      const optionOfAction5 = resp.properties.Table.fields.Action5.options;
+      const sortedOptions = Object.keys(optionOfAction5).map(function(key) {
+          return optionOfAction5[key];
+
+      }).sort(function(a, b) {
+          return (a.index < b.index) ? -1 : 1;  
+      });
+
+      const tableData = event.record['Table'].value;
+      for(let i =0; i<Object.keys(sortedOptions).length; i++){
+        let columnData={
+          value:{
+            "Action5":{
+              type:"DROP_DOWN",
+              value:sortedOptions[i].label
+            },
+            "状況":{
+              type:"CHECK_BOX",
+              value:['未振り返り']
+            },
+            "課題":{
+              type:"MULTI_LINE_TEXT",
+              value:"" 
+            }
+              
+          }
         };
-        return   kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', params).then((resp) => {
-            const optionOfAction5 = resp.properties.Table.fields.Action5.options;
-            const sortedOptions = Object.keys(optionOfAction5).map(function(key) {
-                return optionOfAction5[key];
-
-            }).sort(function(a, b) {
-                return (a.index < b.index) ? -1 : 1;  
-            });
-
-            const tableData = event.record['Table'].value;
-            for(let i =0; i<Object.keys(sortedOptions).length; i++){
-                let columnData={
-                    value:{
-                        "Action5":{
-                            type:"DROP_DOWN",
-                            value:sortedOptions[i].label
-                        },
-                        "状況":{
-                            type:"CHECK_BOX",
-                            value:['未振り返り']
-                        },
-                        "課題":{
-                            type:"MULTI_LINE_TEXT",
-                            value:"" 
-                        }
-                        
-                    }
-                };
-                tableData.push(columnData);       
-            };
-            event.record.Table.value.shift();
-            return event;
-        });       
-    });    
+        tableData.push(columnData);       
+      };
+      event.record.Table.value.shift();
+      return event;
+    });       
+  });    
 })();
