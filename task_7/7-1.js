7ffa4c8edc24cf3352373a6ee6a6405b04512ecc
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-08 12:33:35 +0900

    インデントを整形

--- task_7/7-1.js
+++ task_7/7-1.js
@@ -1,41 +1,40 @@
 (() => {
-    'use strict';
-    kintone.events.on(['app.record.create.show','app.record.edit.show'], (event) => {
+  'use strict';
+  kintone.events.on(['app.record.create.show','app.record.edit.show'], (event) => {
 		event.record.重複禁止項目_文字列.disabled = true;
 		return event;
 	});
-    const trigger = [
-        'app.record.create.change.日付', 
-        'app.record.edit.change.日付',
-        'app.record.create.change.サイボウズ製品',
-        'app.record.edit.change.サイボウズ製品',
-        'app.record.create.change.管理番号',
-        'app.record.edit.change.管理番号'
-    ];
+  const trigger = [
+    'app.record.create.change.日付', 
+    'app.record.edit.change.日付',
+    'app.record.create.change.サイボウズ製品',
+    'app.record.edit.change.サイボウズ製品',
+    'app.record.create.change.管理番号',
+    'app.record.edit.change.管理番号'
+  ];
 
-    const products ={
-        'kintone':'KN',
-        'Garoon':'GR',
-        'サイボウズ Office':'OF',
-        'Mailwise':'MW'
-    };
+  const products ={
+    'kintone':'KN',
+    'Garoon':'GR',
+    'サイボウズ Office':'OF',
+    'Mailwise':'MW'
+  };
 
-    kintone.events.on(trigger, (event) => {
-        event.record['重複禁止項目_文字列']['disabled'] = true;
-        const day = event.record['日付'].value;
-        const Editedday = dateFns.format(day, 'YYYYMMDD');
+  kintone.events.on(trigger, (event) => {
+    event.record['重複禁止項目_文字列']['disabled'] = true;
+    const day = event.record['日付'].value;
+    const editedday = dateFns.format(day, 'YYYYMMDD');
 
+    const product = event.record['サイボウズ製品'].value;
+    const editedProductName = products[product];
 
-        const product = event.record['サイボウズ製品'].value;
-        const EditedProductName = products[product];
+    const manageNum = event.record['管理番号'].value;
 
-        const manageNum = event.record['管理番号'].value;
+    const inputData = editedday+'-'+editedProductName+'-'+manageNum;
 
-        const inputData = Editedday+'-'+EditedProductName+'-'+manageNum;
-
-        event.record['重複禁止項目_文字列'].value = inputData;
-        return event;
-    });
+    event.record['重複禁止項目_文字列'].value = inputData;
+    return event;
+  });
 
 
 })(); 
