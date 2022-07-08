fe505d7ccb00583034cdbf17ec2c2415a054c2ec
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-08 12:16:13 +0900

    tdの追加方法を変更

--- task_5/scripts.js
+++ task_5/scripts.js
@@ -10,24 +10,12 @@ const getNews =()=>{
 
             newsDatas.forEach((row) => {
                 const tr = document.createElement('tr');
-                const tdDay = document.createElement('td');
-                const tdCategory = document.createElement('td');
-                const tdContent = document.createElement('td');
-
-                tdDay.textContent = row.day.value;
-
-                tdCategory.textContent = row.category.value;
-                tdCategory.setAttribute('class',`${row.label.value}`);
-
-                tdContent.innerHTML=`<a href=${row.url.value} target=${row.target.value}>${row.content.value}</a>`;
-
-                tr.appendChild(tdDay);
-                tr.appendChild(tdCategory);
-                tr.appendChild(tdContent);
+                tr.innerHTML=`
+                  <td>${row.day.value}</td>
+                  <td class=${row.label.value}>${row.category.value}</td>
+                  <td><a href=${row.url.value} target=${row.target.value}>${row.content.value}</a></td>`;
                 tbody.appendChild(tr);
-
             });
         });
 };
-
 getNews();
