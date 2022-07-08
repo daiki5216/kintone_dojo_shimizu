d220a3e6b9e8ff3d742e8cd480bc14af01110ec4
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-08 10:06:28 +0900

    セミコロンを追加

--- task_9/9-1.js
+++ task_9/9-1.js
@@ -1,17 +1,16 @@
 (() => {
     'use strict';
-    const appId = kintone.app.getId();
-    const params = {
-        "app":appId,
-        "field":"重複禁止項目"
-    }    
-
+        
     kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], (event) => {
+        const appId = kintone.app.getId();
+        const params = {
+            "app":appId,
+        };
         return kintone.api(kintone.api.url('/k/v1/records.json',true),'GET',params).then((res)=>{
             const allRecords = res.records
-            const recordContentsList =[]
+            const recordContentsList =[];
             allRecords.forEach((record) => {
-                recordContentsList.push(record.重複禁止項目.value)
+                recordContentsList.push(record.重複禁止項目.value);
             });
             const inputData = event.record.重複禁止項目.value
             
@@ -19,9 +18,10 @@
                 const checkOverlap = confirm('レコードが重複しています。このまま保存しますか？');
                 if(!checkOverlap){
                     return false
-                }  
-            }
-        })
+                };  
+            };
+            return event;
+        });
        
     });
 
