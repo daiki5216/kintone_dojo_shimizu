feb597b357c04f6d5d032fe03c364890ed1ad16c
daiki5216 <107820383+daiki5216@users.noreply.github.com>
2022-07-11 14:19:23 +0900

    データの定義の仕方（書き方？を変更）

--- task_8/8-1.js
+++ task_8/8-1.js
@@ -4,30 +4,30 @@
   kintone.events.on('app.record.create.show', (event) => {
     const appId = kintone.app.getId();
     const params = {
-        "app":appId
+        'app':appId
     };
     return kintone.api(kintone.api.url('/k/v1/app/form/fields.json', true), 'GET', params).then((resp) => {
       const optionOfAction5 = resp.properties.Table.fields.Action5.options;
       const sortedOptions = [];
       Object.keys(optionOfAction5).forEach((option)=>{
-        sortedOptions[optionOfAction5[option].index] = option
+        sortedOptions[optionOfAction5[option].index] = option;
       });
 
       const tableData = event.record['Table'].value;
       sortedOptions.forEach((row) => {
         const columnData={
           value:{
-            "Action5":{
-              type:"DROP_DOWN",
-              value:row
-            },
-            "状況":{
-              type:"CHECK_BOX",
-              value:['未振り返り']
-            },
-            "課題":{
-              type:"MULTI_LINE_TEXT",
-              value:"" 
+            Action5:{
+                type:'DROP_DOWN',
+                value:row
+              },
+              状況:{
+                type:'CHECK_BOX',
+                value:['未振り返り']
+              },
+              課題:{
+                type:'MULTI_LINE_TEXT',
+                value:'' 
             }
               
           }
